eb4ec3187b06280d6d6a34f3c1b907e2e481c076
Chris Lennon <chrisjameslennon@gmail.com>
2025-04-01 22:34:17 +1300

    clean up index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';  // React 18
+import * as ReactDOM from 'react-dom/client';
 import * as PrimerReact from '@primer/react';
-// import * as esbuild from 'esbuild-wasm';  // Import esbuild for JSX parsing
 
-// Expose React, ReactDOM, PrimerReact, and esbuild globally
+// Expose React, ReactDOM, PrimerReact globally
 window.React = React;
 window.ReactDOM = ReactDOM;
 window.PrimerReact = PrimerReact;
-// window.esbuild = esbuild;  // Expose esbuild to the window
 
 // Expose React 18 root API for usage in the browser
 window.createRoot = ReactDOM.createRoot;
