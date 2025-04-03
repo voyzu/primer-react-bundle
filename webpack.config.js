8199c186881df7ce45a20550c3e60a2dfb2bc87b
Chris Lennon <chrisjameslennon@gmail.com>
2025-04-03 19:03:44 +1300

    add primer-primitives-bundle.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './index.js',
@@ -16,5 +20,25 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'react-primer-bundle.css',
+        }),
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap('CombineCSSFilesPlugin', () => {
+                    const files = glob.sync('node_modules/@primer/primitives/dist/**/*.css');
+                    let combinedCSS = '';
+
+                    files.forEach(file => {
+                        combinedCSS += fs.readFileSync(file, 'utf8') + '\n';
+                    });
+
+                    fs.writeFileSync(path.resolve(__dirname, 'dist/primer-primitives-bundle.css'), combinedCSS);
+                    console.log('✅ Successfully bundled CSS files to dist/primer-primitives-bundle.css');
+                });
+            }
+        }
+    ],
     mode: 'production',
 };
