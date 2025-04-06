91a52ef151b8217e83316c71cbb24d8921633a97
Chris Lennon <chrisjameslennon@gmail.com>
2025-04-07 07:33:32 +1200

    include octigons

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import * as PrimerReact from '@primer/react';
+import * as PrimerOcticons from '@primer/octicons-react'
 import * as PrimerReactExperimental from '@primer/react/experimental'
 
 // Expose React, ReactDOM, PrimerReact globally
@@ -8,6 +9,7 @@ window.React = React;
 window.ReactDOM = ReactDOM;
 window.PrimerReact = PrimerReact;
 window.PrimerReactExperimental = PrimerReactExperimental;
+window.PrimerOcticons = PrimerOcticons;
 
 // Expose React root API for usage in the browser
 window.createRoot = ReactDOM.createRoot;
